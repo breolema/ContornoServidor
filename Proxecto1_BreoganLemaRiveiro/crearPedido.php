cec7b91f38721021139d8da0e39f295c4c70f9e3
“breolema” <breolema1@gmail.com>
2024-02-23 10:27:31 +0100

    gg

--- Proxecto1_BreoganLemaRiveiro/crearPedido.php
+++ Proxecto1_BreoganLemaRiveiro/crearPedido.php
@@ -35,8 +35,44 @@ foreach ($arrayCarrito as $producto) {
     }
 }
 
-$fecha = date('d-m-Y');
+$fecha = date('Y-m-d H:i:s');
 $usuarioActual = $_SESSION["usuario"];
 $sqlUserActual = "SELECT CodUsu FROM usuarios WHERE Nombre='$usuarioActual'";
+$resultUserActual = $conexion->query($sqlUserActual); 
+$totalPrecio=0;
 
+foreach ($arrayCarrito as $producto) {
+    $totalPrecio += $producto['precioFinal'];
+}
+
+if ($resultUserActual->num_rows > 0) {
+    while ($fila = $resultUserActual -> fetch_assoc()) {
+        $codUserActual= $fila["CodUsu"];
+    }
+    $insertarPedido = "INSERT INTO pedidos (Fecha, CodUsuario, PrecioTotal, CodEstado) VALUES ('$fecha', $codUserActual, $totalPrecio, 1)";
+    $resultInsert = $conexion->query($insertarPedido); 
+}
+
+
+$codPedido = mysqli_insert_id($conexion);
+foreach ($arrayCarrito as $producto) {
+    $codProducto = $producto['codprod'];
+    $unidades = $producto['cantidad'];
+    $precio = $producto['precioFinal'];
+
+    $insertarPedidosProducto = "INSERT INTO pedidosproductos (CodPed, CodProd, Unidades, Precio) VALUES ($codPedido, $codProducto, $unidades, $precio)";
+    $resultInsertPedidosProducto = $conexion->query($insertarPedidosProducto); 
+
+    $actualizarStock = "UPDATE productos SET stock = stock - $unidades WHERE CodProd = $codProducto";
+    $resultActualizarStock = $conexion->query($actualizarStock);
+
+    //$descripcion = "Pedido del producto " . $codProducto . " - " . $producto['nombre'] . " con " . $unidades . " unidades";
+   //$sqlInsertarHistorial = "INSERT INTO historialPedidos (CodUsu, Descripcion, Fecha) VALUES ($codUsuario, '$descripcion', '$fechaActual')";
+    //$resultInsertHistorial = $conexion->query($sqlInsertarHistorial); 
+}
+
+unset($_SESSION["arrayCarrito"]);
+
+header("Location: inicio.php");
+exit;
 ?>
